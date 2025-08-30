31f1c81940c3004461c6e464e561c4f8d41ae894
pmariglia <mariglia4423@gmail.com>
2025-08-30 13:58:05 -0400

    bot's name is directly pulled from config

--- fp/run_battle.py
+++ fp/run_battle.py
@@ -171,7 +171,7 @@ async def get_battle_tag_and_opponent(ps_websocket_client: PSWebsocketClient):
         first_msg = split_msg[0]
         if "battle" in first_msg:
             battle_tag = first_msg.replace(">", "").strip()
-            user_name = split_msg[-1].replace("☆", "").replace("‽", "").strip()
+            user_name = FoulPlayConfig.username
             opponent_name = (
                 split_msg[4].replace(user_name, "").replace("vs.", "").strip()
             )
